56cf27a6fc09ff9eb6ee116b80e515cf54026097
Takahiro Kobayashi <k2t.y4kn@gmail.com>
2020-05-04 17:46:07 +0900

    #4 Add:Oauth用にテーブル設計を直した

--- db/migrate/20200504072301_devise_create_users.rb
+++ db/migrate/20200504072301_devise_create_users.rb
@@ -6,6 +6,8 @@ class DeviseCreateUsers < ActiveRecord::Migration[5.2]
       ## Database authenticatable
       t.string :email,              null: false, default: ""
       t.string :encrypted_password, null: false, default: ""
+      t.string :provider, null: false, default: ""
+      t.string :uid, null: false, default: ""
 
       ## Recoverable
       t.string   :reset_password_token
@@ -38,6 +40,7 @@ class DeviseCreateUsers < ActiveRecord::Migration[5.2]
 
     add_index :users, :email,                unique: true
     add_index :users, :reset_password_token, unique: true
+    add_index :users, [:uid, :provider], unique: true
     # add_index :users, :confirmation_token,   unique: true
     # add_index :users, :unlock_token,         unique: true
   end
