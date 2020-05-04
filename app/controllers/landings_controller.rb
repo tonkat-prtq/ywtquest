d2c8690d156f83b7c93cee5380f622da532d5e34
Takahiro Kobayashi <k2t.y4kn@gmail.com>
2020-05-04 16:17:13 +0900

    #1 Add: Landingコントローラ、トップページ作成

---
+++ app/controllers/landings_controller.rb
@@ -0,0 +1,4 @@
+class LandingsController < ApplicationController
+  def home
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  devise_for :models
+  root to: 'landings#home'
   if Rails.env.development?
     mount LetterOpenerWeb::Engine, at: "/letter_opener"
   end
