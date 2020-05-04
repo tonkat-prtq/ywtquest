6d9bc9cab000cd24c6f8345314b364f42ce16657
Takahiro Kobayashi <k2t.y4kn@gmail.com>
2020-05-04 20:54:06 +0900

    #8 Add: マイページ用のrouting

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
     omniauth_callbacks: 'users/omniauth_callbacks',
     registrations: 'users/registrations'
   }
+  resources :users, only: %i(show)
   root to: 'landings#home'
   if Rails.env.development?
     mount LetterOpenerWeb::Engine, at: "/letter_opener"
