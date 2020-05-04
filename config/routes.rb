8131b9b5f8afae920b17f27f746ef11c4a325fae
Takahiro Kobayashi <k2t.y4kn@gmail.com>
2020-05-04 16:14:49 +0900

    #1 Add: mailerのrouting

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,5 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  if Rails.env.development?
+    mount LetterOpenerWeb::Engine, at: "/letter_opener"
+  end
 end
