df3066935969bd5ee92c26dfec817de3049629f3
Takahiro Kobayashi <k2t.y4kn@gmail.com>
2020-05-04 22:38:16 +0900

    #8 Add: Profileカラムを追加

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_05_04_113555) do
+ActiveRecord::Schema.define(version: 2020_05_04_133554) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -38,6 +38,7 @@ ActiveRecord::Schema.define(version: 2020_05_04_113555) do
     t.integer "totaltime", default: 0, null: false
     t.integer "level", default: 1, null: false
     t.integer "exp_point", default: 0, null: false
+    t.text "profile", default: "", null: false
     t.index ["confirmation_token"], name: "index_users_on_confirmation_token", unique: true
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
