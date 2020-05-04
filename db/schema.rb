dcbd5509268341139f242c7ed10d9bdcad63c29c
Takahiro Kobayashi <k2t.y4kn@gmail.com>
2020-05-04 20:35:14 +0900

    #6 Add: Levelカラム追加

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_05_04_110108) do
+ActiveRecord::Schema.define(version: 2020_05_04_113427) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -36,6 +36,7 @@ ActiveRecord::Schema.define(version: 2020_05_04_110108) do
     t.datetime "updated_at", null: false
     t.string "name", default: "", null: false
     t.integer "totaltime", default: 0, null: false
+    t.integer "level", default: 1, null: false
     t.index ["confirmation_token"], name: "index_users_on_confirmation_token", unique: true
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
