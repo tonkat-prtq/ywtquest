d27f2ea83bd9a91b44c47a5ddb0f28b481f09875
Takahiro Kobayashi <k2t.y4kn@gmail.com>
2020-05-04 18:01:55 +0900

    #4 Fix: Routingのスペルミス

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   devise_for :users, controllers: {
-    omniauth_callbacks: 'users/omniauth_callbacks'
+    omniauth_callbacks: 'users/omniauth_callbacks',
     registrations: 'users/registrations'
   }
   root to: 'landings#home'
