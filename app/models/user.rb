e51b657574ab7bf86dad82ebd7e790f8c533e4eb
Takahiro Kobayashi <k2t.y4kn@gmail.com>
2020-05-04 17:22:56 +0900

    #4 Add: omniauthメソッドをモデルに追加

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,4 +5,20 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable,
          :trackable, :confirmable,
          :omniauthable, omniauth_providers: %i(google_oauth2)
+
+  protected
+  # 以下を追加
+    def self.from_omniauth(access_token)
+      data = access_token.info
+      user = User.where(email: data['email']).first
+
+      # Uncomment the section below if you want users to be created if they don't exist
+      unless user
+          user = User.create(name: data['name'],
+            email: data['email'],
+            password: Devise.friendly_token[0,20]
+          )
+      end
+      user
+    end
 end
