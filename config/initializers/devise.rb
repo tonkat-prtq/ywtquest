70605936ea832244177c55cbb1e1c84b3e34404a
Takahiro Kobayashi <k2t.y4kn@gmail.com>
2020-05-04 14:01:33 +0900

    Add: OAuth認証に必要な値を読み込む

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -296,4 +296,5 @@ Devise.setup do |config|
   # When set to false, does not sign a user in automatically after their password is
   # changed. Defaults to true, so a user is signed in automatically after changing a password.
   # config.sign_in_after_change_password = true
+  config.omniauth :google_oauth2, ENV['GOOGLE_APP_ID'], ENV['GOOGLE_APP_SECRET'], name: :google
 end
