45e895ee66469496789c467235bb8cd54b2e75eb
Takahiro Kobayashi <k2t.y4kn@gmail.com>
2020-05-04 23:30:37 +0900

    #8 Del: いらない記述を削除

--- app/models/user.rb
+++ app/models/user.rb
@@ -33,19 +33,4 @@ class User < ApplicationRecord
     user.save
     user
   end
-
-  # 以下を追加
-    # def self.from_omniauth(access_token)
-    #   data = access_token.info
-    #   user = User.where(email: data['email']).first
-
-    #   # Uncomment the section below if you want users to be created if they don't exist
-    #   unless user
-    #       user = User.create(name: data['name'],
-    #         email: data['email'],
-    #         password: Devise.friendly_token[0,20]
-    #       )
-    #   end
-    #   user
-    # end
 end
