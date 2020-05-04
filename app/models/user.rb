e732ce592de65123ba436e2a3e0742174a0402ca
Takahiro Kobayashi <k2t.y4kn@gmail.com>
2020-05-04 13:57:42 +0900

    Add: OAuth使用のためモジュール追加

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,8 +2,22 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
+         :recoverable, :rememberable, :validatable,
+         :omniauthable, omniauth_providers: %i(google)
   def self.create_unique_string # ランダムなuidを作成する
     SecureRandom.uuid
+  end
+  def self.find_for_google(auth)
+    user = User.find_by(email: auth.info.email)
+    unless user
+      user = User.new(
+        email: auth.info.email,
+        provider: auth.provider,
+        uid:      auth.uid,
+        password: Devise.friendly_token[0, 20],
+      )
+    end
+    user.save
+    user
   end
 end
