e1d0de3846c89396273ad99679b7bbecaa629a31
Takahiro Kobayashi <k2t.y4kn@gmail.com>
2020-05-04 16:39:26 +0900

    #1 Add: rootパス変更、trackableとconfirmable追加

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   devise_for :users
-  get 'landings/home'
+  root to: 'landings#home'
   if Rails.env.development?
     mount LetterOpenerWeb::Engine, at: "/letter_opener"
   end
