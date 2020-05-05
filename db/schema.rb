bda8c8b7fd880209648d67870bf547da60223061
Takahiro Kobayashi <k2t.y4kn@gmail.com>
2020-05-05 13:05:42 +0900

    #11 Add: LogとTodosを一対多でテーブル関連付け

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_05_05_040425) do
+ActiveRecord::Schema.define(version: 2020_05_05_040520) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -47,6 +47,8 @@ ActiveRecord::Schema.define(version: 2020_05_05_040425) do
     t.date "when_to_do"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.bigint "log_id"
+    t.index ["log_id"], name: "index_todos_on_log_id"
   end
 
   create_table "users", force: :cascade do |t|
@@ -81,4 +83,5 @@ ActiveRecord::Schema.define(version: 2020_05_05_040425) do
 
   add_foreign_key "dones", "logs"
   add_foreign_key "knowledges", "logs"
+  add_foreign_key "todos", "logs"
 end
