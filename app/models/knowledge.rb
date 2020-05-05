2b005a279b7f795051b46e98d82b9d192a736fea
Takahiro Kobayashi <k2t.y4kn@gmail.com>
2020-05-05 12:29:36 +0900

    #11 Add: knowledgeテーブル作成

---
+++ app/models/knowledge.rb
@@ -0,0 +1,2 @@
+class Knowledge < ApplicationRecord
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_05_05_032406) do
+ActiveRecord::Schema.define(version: 2020_05_05_032854) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -19,6 +19,13 @@ ActiveRecord::Schema.define(version: 2020_05_05_032406) do
     t.string "title", default: "", null: false
     t.text "comment", default: "", null: false
     t.integer "worktime", default: 0, null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "knowledges", force: :cascade do |t|
+    t.string "title", default: "", null: false
+    t.text "comment", default: "", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
