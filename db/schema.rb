689a02544ef0a54d7d24b789a5f97b122fd819b0
Takahiro Kobayashi <k2t.y4kn@gmail.com>
2020-05-05 12:58:11 +0900

    #11 Add:Logテーブル作成

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_05_05_034652) do
+ActiveRecord::Schema.define(version: 2020_05_05_035405) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -26,6 +26,13 @@ ActiveRecord::Schema.define(version: 2020_05_05_034652) do
   create_table "knowledges", force: :cascade do |t|
     t.string "title", default: "", null: false
     t.text "comment", default: "", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "logs", force: :cascade do |t|
+    t.date "started_on", default: -> { "now()" }, null: false
+    t.date "ended_on"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
