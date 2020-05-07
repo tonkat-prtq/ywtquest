64a782e11b5a87f2c7f9bbb32794e0476369ace2
Takahiro Kobayashi <k2t.y4kn@gmail.com>
2020-05-07 20:15:25 +0900

    #13 Fix: todosテーブルのcommentカラムがいらないことに気付いたので削除

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_05_05_040520) do
+ActiveRecord::Schema.define(version: 2020_05_07_111410) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -43,7 +43,6 @@ ActiveRecord::Schema.define(version: 2020_05_05_040520) do
 
   create_table "todos", force: :cascade do |t|
     t.string "title", default: "", null: false
-    t.text "comment", default: "", null: false
     t.date "when_to_do"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
