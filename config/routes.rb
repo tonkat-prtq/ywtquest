8581f54064212249a8ad496eae5a3f1b67da94cb
Takahiro Kobayashi <k2t.y4kn@gmail.com>
2020-05-07 23:53:48 +0900

    #13 Add: confirm等のルーティング追加

--- config/routes.rb
+++ config/routes.rb
@@ -4,8 +4,19 @@ Rails.application.routes.draw do
     registrations: 'users/registrations'
   }
   resources :users, only: %i(show)
-  resources :logs, only: %i(create new index)
+
+  resources :logs do
+    collection do
+      post :confirm
+      patch :confirm
+    end
+    member do
+      patch :confirm
+    end
+  end
+
   root to: 'landings#home'
+
   if Rails.env.development?
     mount LetterOpenerWeb::Engine, at: "/letter_opener"
   end
