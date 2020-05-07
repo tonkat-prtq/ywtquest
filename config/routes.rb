a9100e43eab8341f7c60370309376a6189187fd8
Takahiro Kobayashi <k2t.y4kn@gmail.com>
2020-05-07 17:10:38 +0900

    #13 Add:new createルーティングだけ追加

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,7 @@ Rails.application.routes.draw do
     registrations: 'users/registrations'
   }
   resources :users, only: %i(show)
+  resources :logs, only: %i(create new)
   root to: 'landings#home'
   if Rails.env.development?
     mount LetterOpenerWeb::Engine, at: "/letter_opener"
