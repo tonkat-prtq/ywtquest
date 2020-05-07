8c5b65499e30121b582cc1ecaa65b9afbe2c8394
Takahiro Kobayashi <k2t.y4kn@gmail.com>
2020-05-07 18:25:04 +0900

    #13 Add: logの登録と同時にDonesの記録に成功

--- app/controllers/logs_controller.rb
+++ app/controllers/logs_controller.rb
@@ -2,19 +2,23 @@ class LogsController < ApplicationController
   def index
     @logs = Log.all
   end
-  
+
   def new
     @log = Log.new
+    dones = @log.dones.build
   end
 
   def create
-    @log = Log.new(log_params)
-    @log.save
+    @log = Log.create(log_params)
   end
 
   private
 
   def log_params
-    params.require(:log).permit(:started_on, :ended_on)
+    params.require(:log).permit(:started_on, :ended_on, 
+      dones_attributes: [:title, :comment, :worktime],
+      knowledges: [:title, :comment],
+      todos: [:titile, :comment, :when_to_do]
+    )
   end
 end
--- app/models/log.rb
+++ app/models/log.rb
@@ -1,5 +1,6 @@
 class Log < ApplicationRecord
-  has_many :dones
-  has_many :knowledges
-  has_many :todos
+  has_many :dones, inverse_of: :log, dependent: :destroy # inverse_ofで親と子が同時に作成された時外部キーが無い事によるid違反を解消してくれる
+  has_many :knowledges, inverse_of: :log, dependent: :destroy # +子から親を辿れるようになる
+  has_many :todos, inverse_of: :log, dependent: :destroy # +発行されるSQLが少なくなる
+  accepts_nested_attributes_for :dones # 親のレコードを更新時に子のレコードも保存される
 end
