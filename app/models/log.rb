75f272db426605f27952c02d4c3039e0fb11b920
Takahiro Kobayashi <k2t.y4kn@gmail.com>
2020-05-07 23:53:40 +0900

    #13 Fix: Inverse_ofを削除してみる

--- app/models/log.rb
+++ app/models/log.rb
@@ -1,6 +1,6 @@
 class Log < ApplicationRecord
-  has_many :dones, inverse_of: :log, dependent: :destroy # inverse_ofで親と子が同時に作成された時外部キーが無い事によるid違反を解消してくれる
-  has_many :knowledges, inverse_of: :log, dependent: :destroy # +子から親を辿れるようになる
-  has_many :todos, inverse_of: :log, dependent: :destroy # +発行されるSQLが少なくなる
+  has_many :dones, dependent: :destroy # inverse_ofで親と子が同時に作成された時外部キーが無い事によるid違反を解消してくれる
+  has_many :knowledges, dependent: :destroy # +子から親を辿れるようになる
+  has_many :todos, dependent: :destroy # +発行されるSQLが少なくなる
   accepts_nested_attributes_for :dones, :knowledges, :todos # 親のレコードを更新時に子のレコードも保存される
 end
