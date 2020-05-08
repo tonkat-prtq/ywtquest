58a7671d452c44cbddb189b40ea09fea4c0df6b2
Takahiro Kobayashi <k2t.y4kn@gmail.com>
2020-05-08 18:11:59 +0900

    #13 Add: UserとLogのアソシエーション後、createが行われるように

--- app/controllers/logs_controller.rb
+++ app/controllers/logs_controller.rb
@@ -5,22 +5,32 @@ class LogsController < ApplicationController
   end
 
   def new
-    @log = Log.new
+    @log = Log.new # current_user.logs.build(log_params)からこう変更して変なパラメーターが渡っちゃうエラー解決
     @dones = @log.dones.build
     @knowledges = @log.knowledges.build
     @todos = @log.todos.build
   end
 
   def create
-    @log = Log.create(log_params)
-    redirect_to logs_path
+    @log = current_user.logs.build(log_params)
+    if params[:back]
+      render 'new'
+    else
+      if @log.save
+        redirect_to logs_path, flash: {success: "YWTを作成しました"}
+        # PostMailer.post_mail(current_user.email).deliver
+      else
+        render :new
+      end
+    end
   end
 
   def show
   end
 
   def edit
-    # if current_user.id == @log.user.id あとでつける
+    redirect_to logs_path, flash: {danger: "自分の記事以外の編集はできません"} unless current_user.id == @log.user.id
+    binding.pry
   end
 
   def update
@@ -36,16 +46,25 @@ class LogsController < ApplicationController
   end
 
   def confirm
-    binding.pry
-    @log = Log.new(log_params)
+    @log = current_user.logs.build(log_params)
     @log.id = params[:id]
+    binding.pry
+  end
+
+  def destroy
+    if current_user.id == @log.user.id
+      @log.destroy
+      redirect_to logs_path, flash: {success: "YWTを削除しました"}
+    else
+      redirect_to logs_path, flash: {danger: "不正な操作です"}
+    end
   end
 
 
   private
 
   def log_params
-    params.require(:log).permit(:id, :started_on, :ended_on, 
+    params.require(:log).permit(:id, :started_on, :ended_on, :user_id,
       dones_attributes: [:title, :comment, :worktime, :logs_id],
       knowledges_attributes: [:title, :comment, :logs_id],
       todos_attributes: [:title, :when_to_do, :logs_id]
--- app/models/log.rb
+++ app/models/log.rb
@@ -1,4 +1,5 @@
 class Log < ApplicationRecord
+  belongs_to :user
   has_many :dones, dependent: :destroy # inverse_ofで親と子が同時に作成された時外部キーが無い事によるid違反を解消してくれる
   has_many :knowledges, dependent: :destroy # +子から親を辿れるようになる
   has_many :todos, dependent: :destroy # +発行されるSQLが少なくなる
