e82a15d74f4becdb7d76af293ab37ce92788f6ac
Takahiro Kobayashi <k2t.y4kn@gmail.com>
2020-05-09 20:14:15 +0900

    #15 Add: modelにcocoon用のフォーム削除設定追加

--- app/models/log.rb
+++ app/models/log.rb
@@ -3,5 +3,5 @@ class Log < ApplicationRecord
   has_many :dones, dependent: :destroy # inverse_ofで親と子が同時に作成された時外部キーが無い事によるid違反を解消してくれる
   has_many :knowledges, dependent: :destroy # +子から親を辿れるようになる
   has_many :todos, dependent: :destroy # +発行されるSQLが少なくなる
-  accepts_nested_attributes_for :dones, :knowledges, :todos # 親のレコードを更新時に子のレコードも保存される
+  accepts_nested_attributes_for :dones, :knowledges, :todos, reject_if: :all_blank, allow_destroy: true # 親のレコードを更新時に子のレコードも保存される
 end
