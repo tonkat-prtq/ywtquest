fdbe0df83873430a076c124956b129006f7326fb
Takahiro Kobayashi <k2t.y4kn@gmail.com>
2020-05-12 18:30:55 +0900

    #17 Add: strong parameterにtag_list追加

--- app/controllers/logs_controller.rb
+++ app/controllers/logs_controller.rb
@@ -72,7 +72,7 @@ class LogsController < ApplicationController
   private
 
   def log_params
-    params.require(:log).permit(:id, :started_on, :ended_on, :user_id,
+    params.require(:log).permit(:id, :started_on, :ended_on, :user_id, :tag_list,
       dones_attributes: [:id, :title, :comment, :worktime, :logs_id, :_destroy],
       knowledges_attributes: [:id, :title, :comment, :logs_id, :_destroy],
       todos_attributes: [:id, :title, :when_to_do, :logs_id, :_destroy]
