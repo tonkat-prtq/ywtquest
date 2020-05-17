94c7ab59bb39346a91d957a4dc01fbdee11fb744
Takahiro Kobayashi <k2t.y4kn@gmail.com>
2020-05-17 15:05:17 +0900

    #30 Add: ended_on > started_onのバリデーション

--- app/models/log.rb
+++ app/models/log.rb
@@ -5,4 +5,11 @@ class Log < ApplicationRecord
   has_many :knowledges, inverse_of: :log, dependent: :destroy # +子から親を辿れるようになる
   has_many :todos, inverse_of: :log, dependent: :destroy # +発行されるSQLが少なくなる
   accepts_nested_attributes_for :dones, :knowledges, :todos, reject_if: :all_blank, allow_destroy: true # 親のレコードを更新時に子のレコードも保存される
+
+  validate :ended_on_after_started_on
+
+  def ended_on_after_started_on
+    errors.add(:ended_on, "は開始日と同じか、それ以降の日付を選択してください") if
+    started_on > ended_on
+  end
 end
