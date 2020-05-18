8d467c2c81449c3b31d712c5b33030fe02344164
Takahiro Kobayashi <k2t.y4kn@gmail.com>
2020-05-18 14:22:05 +0900

    #31 Add: メール認証後はログインした状態で戻ってこれるように

--- app/controllers/users/confirmations_controller.rb
+++ app/controllers/users/confirmations_controller.rb
@@ -27,4 +27,11 @@ class Users::ConfirmationsController < Devise::ConfirmationsController
   # def after_confirmation_path_for(resource_name, resource)
   #   super(resource_name, resource)
   # end
+
+  private
+
+  def after_confirmation_path_for(_resource_name, resource)
+    sign_in(resource)
+    logs_path
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
   devise_for :users, controllers: {
     omniauth_callbacks: 'users/omniauth_callbacks',
-    registrations: 'users/registrations'
+    registrations: 'users/registrations',
+    confirmations: 'users/confirmations'
   }
   resources :users, only: %i(show)
 
