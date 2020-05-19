ca8144b8463e27a5d9ce4228636b087791a85ded
Takahiro Kobayashi <k2t.y4kn@gmail.com>
2020-05-19 22:55:46 +0900

    #43 Fix: コントローラのエラー時例外処理する条件分岐変更

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -12,7 +12,7 @@ class ApplicationController < ActionController::Base
     devise_parameter_sanitizer.permit :sign_in, keys: added_attrs
   end
 
-  unless Rails.env.production?
+  unless Rails.env.development?
     rescue_from Exception,                      with: :render_500
     rescue_from ActiveRecord::RecordNotFound,   with: :render_404
     rescue_from ActionController::RoutingError, with: :render_404
