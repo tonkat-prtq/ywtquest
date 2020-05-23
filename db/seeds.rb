739be095dea872112f264812ea974eda56ef96ff
Takahiro Kobayashi <k2t.y4kn@gmail.com>
2020-05-23 18:43:18 +0900

    Add: seedデータ

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,3 +6,39 @@ CSV.foreach('db/level_setting(仮).csv', headers: true) do |row|
     threshold: row[1]
   )
 end
+
+firstuser = User.new(
+  name: "first user",
+  email: "firstuser@test.com",
+  password: "password",
+  id: 1
+)
+
+firstuser.skip_confirmation!
+firstuser.save!
+
+20.times do |no|
+  Log.create(
+    started_on: Time.now,
+    ended_on: Time.now + 1.days,
+    user_id: 1
+  )
+
+  Done.create(
+    title: "やったこと #{no}",
+    comment: "これやった",
+    log_id: no
+  )
+
+  Knowledge.create(
+    title: "わかったこと #{no}",
+    comment: "これがわかった",
+    log_id: no
+  )
+
+  Todo.create(
+    title: "次やること #{no}",
+    when_to_do: Time.now + 3.days,
+    log_id: no
+  )
+end
