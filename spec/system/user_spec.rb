879bd04cfe79e5d43c567045d172a64ac756ff64
Takahiro Kobayashi <k2t.y4kn@gmail.com>
2020-05-25 20:16:40 +0900

    #76 Problem: メール認証のSystem Specの書き方が分からない

--- spec/system/user_spec.rb
+++ spec/system/user_spec.rb
@@ -5,7 +5,14 @@ RSpec.describe 'Users', type: :system do
     context 'ユーザーのデータがなくログインしていない場合' do
       example 'ユーザー新規登録が出来る' do
         visit new_user_registration_path
-        expect(page).to have_content '登録する'
+        fill_in 'user[name]', with: 'firstUser'
+        fill_in 'user[email]', with: 'firstuser@test.com'
+        fill_in 'user[password]', with: 'password'
+        fill_in 'user[password_confirmation]', with: 'password'
+        click_button '登録する'
+        expect { click_button '登録する' }.to change { ActionMailer::Base.deliveries.size }.by(1)
+        # click_on 'メールアドレスの確認'
+        # expect(page).to have_content 'メールアドレスが確認できました。'
       end
     end
   end
