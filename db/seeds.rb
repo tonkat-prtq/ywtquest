2206d2f6d35316d123e355ffa43531a64a498986
Takahiro Kobayashi <k2t.y4kn@gmail.com>
2020-06-15 21:51:24 +0900

    :poultry_leg: 旧シードデータ用のコードを削除

--- db/seeds.rb
+++ db/seeds.rb
@@ -239,30 +239,4 @@ unless Rails.env.test? # テスト環境では以下のseedデータは不要
     ]
   )
 
-  # 20.times do |no|
-  #   Log.create(
-  #     started_on: Time.now,
-  #     ended_on: Time.now + 1.days,
-  #     user_id: 1
-  #   )
-
-  #   Done.create(
-  #     title: "やったこと #{no}",
-  #     comment: "これやった",
-  #     log_id: no
-  #   )
-
-  #   Knowledge.create(
-  #     title: "わかったこと #{no}",
-  #     comment: "これがわかった",
-  #     log_id: no
-  #   )
-
-  #   Todo.create(
-  #     title: "次やること #{no}",
-  #     when_to_do: Time.now + 3.days,
-  #     log_id: no
-  #   )
-
-  # end
 end
