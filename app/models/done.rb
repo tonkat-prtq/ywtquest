60307c2c808f1af1a61ea09856db923dbf3d639d
Takahiro Kobayashi <k2t.y4kn@gmail.com>
2020-06-22 23:03:45 +0900

    :bird: #107 modelでworktimeのマイナス入力を許さないバリデーション追加

--- app/models/done.rb
+++ app/models/done.rb
@@ -3,4 +3,5 @@ class Done < ApplicationRecord
   validates :title, presence: true, length: {maximum: 255}
   validates :comment, length: {maximum: 5000}
   validates :worktime, length: {maximum: 5}
+  validates :worktime, numericality: { greater_than_or_equal_to: 0}
 end
