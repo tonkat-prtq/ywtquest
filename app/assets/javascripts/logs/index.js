d5e68b0c4ba2643420f3031ab846bb447edf25cd
Takahiro Kobayashi <k2t.y4kn@gmail.com>
2020-06-29 22:03:21 +0900

    :grapes: #66 クリックした要素の矢印のみ回転するようにthisを使って修正

--- app/assets/javascripts/logs/index.js
+++ app/assets/javascripts/logs/index.js
@@ -1,13 +1,13 @@
 $(function(){
   $('.card-header').click(function(){
     var icon = $(this).find('.fa-chevron-down');
-    console.log(icon[0]);
 
-    if(icon !== undefined) { // 本文がなく、アイコンがないcard-headerをクリックした時はundefinedが返されることを利用した
-      console.log("true");
-      $('.fa-chevron-down').toggleClass("spined-icon")
-    } else {
-      console.log("false");
+    // 本文がなく、アイコンがないcard-headerをクリックした時はundefinedが返されることを利用した
+    if(icon !== undefined) { 
+      // thisをつけてクリックした要素にだけ効かせる。そうしないと全部の矢印にtoggleClassが効いてアニメーションしてしまう
+      // findで.fa-chevron-downのついた要素を見つけ、そこにクラスを追加している
+      // spined-iconというクラスでアニメーションをつけている
+      $(this).find('.fa-chevron-down').toggleClass("spined-icon")
     }
   });
 });
