3ec7fedfa579405631afeafddf86e1e8590fa352
Takahiro Kobayashi <k2t.y4kn@gmail.com>
2020-06-30 00:40:31 +0900

    :grapes: #66 空行を削除、条件文の修正

--- app/assets/javascripts/logs/index.js
+++ app/assets/javascripts/logs/index.js
@@ -1,13 +1,13 @@
 $(function(){
   $('.card-header').click(function(){
     var icon = $(this).find('.fa-chevron-down');
-
     // 本文がなく、アイコンがないcard-headerをクリックした時はundefinedが返されることを利用した
-    if(icon !== undefined) { 
+    if(icon[0] !== undefined) { 
       // thisをつけてクリックした要素にだけ効かせる。そうしないと全部の矢印にtoggleClassが効いてアニメーションしてしまう
       // findで.fa-chevron-downのついた要素を見つけ、そこにクラスを追加している
       // spined-iconというクラスでアニメーションをつけている
       $(this).find('.fa-chevron-down').toggleClass("spined-icon")
+      console.log(icon[0])
     }
   });
 });
