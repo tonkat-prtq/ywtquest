2e26399024c2ae8f857473dc476233428c9a7b0a
Takahiro Kobayashi <k2t.y4kn@gmail.com>
2020-06-30 17:50:59 +0900

    :bird: #120 YWTの各モデルに降順並び変えのためのscopeを記述

--- app/models/todo.rb
+++ app/models/todo.rb
@@ -1,4 +1,6 @@
 class Todo < ApplicationRecord
   belongs_to :log
   validates :title, presence: true, length: {maximum: 255}
+
+  scope :default_order, -> { order(created_at: :desc) } # 作成日を降順に並べるscopeの名前を変更し分かりやすく
 end
